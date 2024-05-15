11b69ee8c03832c9f044394cea0def63be96ad43
2110030392santhosh <2010030005@klh.edu.in>
2024-05-16 00:55:18 +0530

    CryptNet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import './App.css'
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://randomuser.me/api/?page=1&results=1&seed=abc');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const result = await response.json();
+        setData(result.results[0]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='container'>
+      <div className='image-container'>
+        {data && (
+          <img src={data.picture.medium} alt="alt"/>
+        )}
+      </div>
+      <div className='content'>
+        <div className='names'>
+        <div className='firstName'>
+          {data && (
+          <h3>{data.name.first}</h3>
+          )}
+        </div>
+        <div className='lastName'>
+        {data && (
+          <h3>{data.name.last}</h3>
+          )}
+        </div>
+        </div>
+        <div className='Gender'>
+        {data && (
+          <h3>{data.gender}</h3>
+          )}
+        </div>
+        <div className='phoneNumber'>
+        {data && (
+          <h3>{data.phone}</h3>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
